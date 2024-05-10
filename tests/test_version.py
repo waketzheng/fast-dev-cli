848a82e3a79b473f8d24fed22738643d31e5455f
Waket Zheng <waketzheng@gmail.com>
2024-05-10 10:13:54 +0800

    test version value in init file

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,11 @@
+import subprocess
+
+from fast_dev_cli import __version__
 from fast_dev_cli.cli import get_current_version, version
 
 
 def test_version(capsys):
     version()
     assert get_current_version() in capsys.readouterr().out
+    r = subprocess.run(["poetry", "version", "-s"], capture_output=True)
+    assert r.stdout.decode().strip() == __version__
