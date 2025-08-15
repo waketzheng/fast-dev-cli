562d8542c9961d000534c0dacdbb115400f29631
Waket Zheng <waketzheng@gmail.com>
2025-08-15 18:10:51 +0800

    Fixing bump test error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 import pytest
@@ -16,7 +17,7 @@ readme = "README.md"
 packages = [{include = "fast_dev_cli"}]
 
 [tool.poetry.dependencies]
-python = "^3.9"
+python = "^3.10"
 click = ">=7.1.1"  # Many package depends on click, so only limit min version
 strenum = {version = ">=0.4.15", python = "<3.11"}
 type-extensions = {version = ">=0.1.2", python = "<3.11"}
@@ -34,7 +35,10 @@ all = ["ruff", "typer", "mypy", "bumpversion", "pytest", "coverage"]
 coveralls = {version = ">=4.0.0", python = ">=3.10,<3.13"}
 coverage = ">=6.5.0"  # use >= to compare with coveralls
 typer = "^0.12.3"
+ruff = "^0.4.4"
+mypy = "^1.10.0"
 pytest = "^8.2.0"
+ipython = "^8.24.0"
 bumpversion = "^0.6.0"
 pytest-mock = "^3.14.0"
 type-extensions = "^0.1.2"
@@ -55,5 +59,6 @@ def tmp_work_dir(tmp_path):
 @pytest.fixture
 def tmp_poetry_project(tmp_work_dir: Path):
     Path(TOML_FILE).write_text(TOML_CONTENT)
-    run_and_echo("poetry config --local virtualenvs.in-project true")
+    if sys.version_info >= (3, 10):
+        run_and_echo("poetry config --local virtualenvs.in-project true")
     yield tmp_work_dir
