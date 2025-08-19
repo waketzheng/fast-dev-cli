270f7210e89eb86bda0aab9a72448ff5c23d416a
Waket Zheng <waketzheng@gmail.com>
2025-08-19 17:45:56 +0800

    tests: fix help test error

--- tests/test_help.py
+++ tests/test_help.py
@@ -4,4 +4,4 @@ from asynctor import Shell
 def test_help():
     out = Shell("fast").capture_output()
     out_help = Shell("fast --help").capture_output()
-    assert out == out_help
+    assert out.strip() == out_help.strip()
