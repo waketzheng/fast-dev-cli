a3fa7398c4b27aaaf9746da6cb5e930a5d966ef9
Waket Zheng <waketzheng@gmail.com>
2025-08-31 17:06:44 +0800

    tests: verify fast pypi

---
+++ tests/test_pypi.py
@@ -0,0 +1,43 @@
+import shutil
+from pathlib import Path
+
+import pytest
+
+from fast_dev_cli.cli import Exit, pypi
+
+ASSERTS_DIR = Path(__file__).parent / "assets"
+
+
+def test_pypi(tmp_work_dir, capsys):
+    uv_qh = ASSERTS_DIR / "uv.lock"
+    uv_tx = ASSERTS_DIR / "uv-tx.lock"
+    lock_file = Path(uv_qh.name)
+    Path("pyproject.toml").touch()
+    pypi()
+    assert "'uv.lock' not found!" in capsys.readouterr().out
+    shutil.copy(uv_qh, ".")
+    text = uv_qh.read_text("utf-8")
+    with pytest.raises(Exit):
+        pypi()
+    new_text = lock_file.read_text("utf-8")
+    assert new_text != text
+    assert "tsinghua" in text and "pypi.org" not in text
+    assert "tsinghua" not in new_text and "pypi.org" in new_text
+    shutil.copy(uv_qh, ".")
+    pypi(quiet=True)
+    assert new_text == lock_file.read_text("utf-8")
+    pypi()
+    assert new_text == lock_file.read_text("utf-8")
+
+    shutil.copy(uv_tx, lock_file)
+    text = lock_file.read_text("utf-8")
+    with pytest.raises(Exit):
+        pypi()
+    new_text = lock_file.read_text("utf-8")
+    assert new_text != text
+    assert "tencent" in text and "pypi.org" not in text
+    assert "tencent" not in new_text and "pypi.org" in new_text
+    pypi(quiet=True)
+    assert new_text == lock_file.read_text("utf-8")
+    pypi()
+    assert new_text == lock_file.read_text("utf-8")
