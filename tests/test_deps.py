4c4b40c704cd35671ef97bda909b79b4c547f0ab
Waket Zheng <waketzheng@gmail.com>
2025-10-16 21:12:39 +0800

    tests: add more test cases

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -48,3 +48,31 @@ def test_fast_deps():
     assert out == "--> uv sync --active --all-extras --all-groups"
     out = capture_cmd_output("fast deps --uv --dry --no-active --no-inexact")
     assert out == "--> uv sync --all-extras --all-groups"
+
+
+def test_smart_fast_deps(tmp_work_dir, monkeypatch):
+    toml_file = tmp_work_dir.joinpath("pyproject.toml")
+    if toml_file.exists():
+        toml_file.unlink()
+    tmp_work_dir.joinpath("uv.lock").touch()
+    out = capture_cmd_output("fast deps --dry")
+    assert (
+        out
+        == "--> python -m ensurepip && python -m pip install --upgrade pip && python -m pip install -e . --group dev"
+    )
+    toml_file.touch()
+    out = capture_cmd_output("fast deps --dry")
+    assert out == "--> uv sync --inexact --active --all-extras --all-groups"
+    pdm_lock = tmp_work_dir.joinpath("pdm.lock")
+    pdm_lock.touch()
+    out = capture_cmd_output("fast deps --dry")
+    assert out == "--> uv sync --inexact --active --all-extras --all-groups"
+    toml_file.write_text("[tool.pdm]\ndistribution=false")
+    out = capture_cmd_output("fast deps --dry")
+    assert out == "--> uv sync --inexact --active --all-extras --all-groups"
+    monkeypatch.setenv("FASTDEVCLI_SKIP_UV", "1")
+    out = capture_cmd_output("fast deps --dry")
+    assert out == "--> pdm install --frozen -G :all"
+    pdm_lock.unlink()
+    out = capture_cmd_output("fast deps --dry")
+    assert out == "--> pdm install --frozen -G :all"
