3bceab3b7f630ee3200d53ee0f50d18621cf2193
Waket Zheng <waketzheng@gmail.com>
2026-09-19 17:10:30 +0800

    Fix lint issues

--- tests/test_vi.py
+++ tests/test_vi.py
@@ -6,24 +6,26 @@ def test_fast_vi_md() -> None:
     cmd2 = "fast vi README. --dry"
     cmd3 = "fast vi README.md --dry"
     expected = "--> vim README.md"
-    assert capture_cmd_output(cmd) == expected 
-    assert capture_cmd_output(cmd2) == expected 
-    assert capture_cmd_output(cmd3) == expected 
+    assert capture_cmd_output(cmd) == expected
+    assert capture_cmd_output(cmd2) == expected
+    assert capture_cmd_output(cmd3) == expected
+
 
 def test_fast_vi_py() -> None:
     cmd = "fast vi fast_dev_cli/cli --dry"
     cmd2 = "fast vi fast_dev_cli/cli. --dry"
     cmd3 = "fast vi fast_dev_cli/cli.py --dry"
     expected = "--> vim fast_dev_cli/cli.py"
-    assert capture_cmd_output(cmd) == expected 
-    assert capture_cmd_output(cmd2) == expected 
-    assert capture_cmd_output(cmd3) == expected 
+    assert capture_cmd_output(cmd) == expected
+    assert capture_cmd_output(cmd2) == expected
+    assert capture_cmd_output(cmd3) == expected
+
 
 def test_fast_vi_multi() -> None:
     cmd = "fast vi fast_dev_cli/cli README --dry"
     cmd2 = "fast vi fast_dev_cli/cli. README.md --dry"
     cmd3 = "fast vi fast_dev_cli/cli.py README. --dry"
     expected = "--> vim -O fast_dev_cli/cli.py README.md"
-    assert capture_cmd_output(cmd) == expected 
-    assert capture_cmd_output(cmd2) == expected 
-    assert capture_cmd_output(cmd3) == expected 
+    assert capture_cmd_output(cmd) == expected
+    assert capture_cmd_output(cmd2) == expected
+    assert capture_cmd_output(cmd3) == expected
